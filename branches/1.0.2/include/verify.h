4d5042edda487ad1091672c4a19705553d8dec3b
emukidid <emukidid@5d81b4d4-c38b-535d-aecd-ec79a213ef6f>
2010-09-18 16:33:44 +0000

    Tagging Version 1.0.2

---
+++ branches/1.0.2/include/verify.h
@@ -0,0 +1,30 @@
+/**
+ * CleanRip - verify.h
+ * Copyright (C) 2010 emu_kidid
+ *
+ * CleanRip homepage: http://code.google.com/p/cleanrip/
+ * email address: emukidid@gmail.com
+ *
+ *
+ * This program is free software; you can redistribute it and/
+ * or modify it under the terms of the GNU General Public Li-
+ * cence as published by the Free Software Foundation; either
+ * version 2 of the Licence, or any later version.
+ *
+ * This program is distributed in the hope that it will be use-
+ * ful, but WITHOUT ANY WARRANTY; without even the implied war-
+ * ranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public Licence for more details.
+ *
+ **/
+
+#ifndef VERIFY_H
+#define VERIFY_H
+
+void verify_init(char *mountPath);
+int verify_findMD5Sum(const char * md5, int disc_type);
+int verify_is_available(int disc_type);
+char *verify_get_name();
+
+#endif
+
